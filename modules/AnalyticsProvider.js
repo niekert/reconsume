cf614461c5a0a9c765a46319f5c738c3848d2092
Niek Kruse <niekkruse70@gmail.com>
2018-02-03 15:46:31 +0100

    Improve analyticsprovider

--- modules/AnalyticsProvider.js
+++ modules/AnalyticsProvider.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider, Consumer } from './AnalyticsContext';
 
-class AnalyticsProvider extends React.Component {
-  render() {
-    const { children, ...props } = this.props;
+function AnalyticsProvider({ children, ...props }) {
+  return (
+    <Consumer quiet>
+      {(analytics) => {
+        const mergedAnalytics = {
+          ...analytics,
+          ...props.analytics,
+        };
 
-    return (
-      <Consumer>
-        {(analytics) => {
-          const mergedAnalytics = {
-            ...analytics,
-            ...this.props.analytics,
-          };
-          return (
-            <Provider value={mergedAnalytics}>
-              {children({ analytics: mergedAnalytics, ...props })}
-            </Provider>
-          );
-        }}
-      </Consumer>
-    );
-  }
+        return (
+          <Provider value={mergedAnalytics}>
+            {children({ ...props, analytics: mergedAnalytics })}
+          </Provider>
+        );
+      }}
+    </Consumer>
+  );
 }
+
+AnalyticsProvider.propTypes = {
+  children: PropTypes.func.isRequired,
+  analytics: PropTypes.object, // eslint-disable-line
+};
+
+AnalyticsProvider.defaultProps = {
+  analytics: {},
+};
 
 export default AnalyticsProvider;
--- modules/withAnalytics.js
+++ modules/withAnalytics.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AnalayticsProvider from './AnalyticsProvider';
 
-const withAnalytics = analytics => ComposedComponent => props => (
-  <AnalayticsProvider analytics={analytics}>
-    {mergedAnalytics => <ComposedComponent {...props} analytics={mergedAnalytics} />}
-  </AnalayticsProvider>
-);
+const withAnalytics = input => ComposedComponent => (props) => {
+  const analytics = typeof input === 'function' ? input(props) : input;
+
+  return (
+    <AnalayticsProvider analytics={analytics}>
+      {mergedAnalytics => <ComposedComponent {...props} analytics={mergedAnalytics} />}
+    </AnalayticsProvider>
+  );
+};
 
 export default withAnalytics;
